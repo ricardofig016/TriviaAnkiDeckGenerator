fe5a1b37ae3318288eeb3e70d708cf8304ccc4ba
ricardofig016 <ricardifigueiredo016@gmail.com>
2023-08-10 18:32:06 +0100

    finished project

--- main.py
+++ main.py
@@ -1,53 +1,81 @@
 import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import genanki
 
 url = "https://www.randomtriviagenerator.com/"
 driver = webdriver.Chrome()
-
-answer = []
+question = set()
 
 try:
-    while len(answer) < 1000:
+    while len(question) < 30000:
         driver.get(url)
 
-        time.sleep(5)
-
         soup = BeautifulSoup(driver.page_source, "html.parser")
-
         cards = soup.find_all("md-card-content")
+        subjects = soup.find_all(
+            "div",
+            class_="Card_category-text__1i-2p col-center padding-sm padding-left-none padding-right-none xl layout-column flex",
+        )
+        for i in range(0, len(subjects), 2):
+            q = [
+                subjects[i].get_text(),
+                cards[i].get_text(),
+                cards[i + 1].get_text(),
+            ]
+            question.add(tuple(q))
 
-        for i in range(0, len(cards), 2):
-            ans = [cards[i].get_text(), cards[i + 1].get_text()]
-            if not ans in answer:
-                answer.append(ans)
-        print(len(answer))
+        print(f"questions scraped: {len(question)}")
 
 except KeyboardInterrupt:
     pass
 
-print(answer)
-
 driver.quit()
 
+guess_answer_html = """<p><b>{{Answer}}</b></p>"""
+
 model = genanki.Model(
-    model_id="Trivia_Question_Model",
+    model_id=20983477,
     name="Trivia Question",
     fields=[{"name": "Subject"}, {"name": "Question"}, {"name": "Answer"}],
     templates=[
         {
-            "name": "{{Subject}}",
+            "name": "Trivia",
             "qfmt": "{{Question}}",
-            "afmt": '{{FrontSide}}<hr id="answer">{{Answer}}',
+            "qfmt": '<p class="light">{{Subject}}</p>  <p><b>{{Question}}</b></p>',
+            "afmt": '{{FrontSide}} <hr id="answer"> ' + guess_answer_html,
         }
     ],
+    css="""
+            .card {
+                font-family: arial;
+                font-size: 18px;
+                text-align: left;
+                color: black;
+                background-color: white;
+                line-height:1.45;
+                text-align:center;
+                background-color:#f9f9f9;
+            }
+            .small {
+                font-size:13px;
+                font-weight:bold;
+            }
+            .light {
+                color:#b0b0b5;
+            }
+            img {
+                border:2px solid white;
+            }
+        """,
 )
 
 deck = genanki.Deck(29348752, "Trivia Deck")
 
-for ans in answer:
-    note = genanki.Note(model=model, fields=["Subject", ans[0], ans[1]])
+for q in question:
+    note = genanki.Note(model=model, fields=[q[0], q[1], q[2]])
     deck.add_note(note)
 
 package = genanki.Package(deck)
